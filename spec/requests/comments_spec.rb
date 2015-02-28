491148f6a33e641e793b22f4060c7ac47b9f6fa5
akahigeg <akahigeg@gmail.com>
2015-02-28 18:00:30 +0900

    comment api response includes only accepted comments

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -4,25 +4,51 @@ describe "Comments API" do
   describe "GET /comments" do
     before do
       Comment.delete_all
-      Comment.create(location: '/path1', username: 'foo', content: 'funny comment')
-      Comment.create(location: '/path1', username: 'bar', content: 'much funny comment')
+      Comment.create(location: '/path1', username: 'foo', content: 'funny comment', accepted: true)
+      Comment.create(location: '/path1', username: 'bar', content: 'much funny comment', accepted: true)
+      Comment.create(location: '/path1', username: 'hoge', content: 'unaccepted comment', accepted: false)
       Comment.create(location: '/path2', username: 'someone', content: 'comment for another path')
-
-      get "/comments", {location: '/path1'}, { "HTTP_ACCEPT" => "application/json" }
-      @comments = JSON.parse(response.body)
     end
 
-    it "returns all the comments for a location" do
-      expect(response.status).to eq 200
+    context "requested with valid client_key" do
+      before do
+        get "/comments", {location: '/path1', client_key: Setting::client_key}, { "HTTP_ACCEPT" => "application/json" }
+        @comments = JSON.parse(response.body)
+      end
+
+      it "returns http status 200" do
+        expect(response.status).to eq 200
+      end
+
+      it "returns all accepted comments for a location" do
+        expect(@comments.count).to eq 2
+      end
+
+      it "contains collect usernames" do
+        usernames = @comments.map { |c| c["username"] }
+        expect(usernames).to match_array(["foo", "bar"])
+      end
     end
 
-    it "returns 2 comments" do
-      expect(@comments.count).to eq 2
+    context "requested invalid client_key" do
+      before do
+        get "/comments", {location: '/path1', client_key: "invalidkey"}, { "HTTP_ACCEPT" => "application/json" }
+      end
+
+      it "returns http status 404" do
+        expect(response.status).to eq 404
+      end
     end
 
-    it "contains collect usernames" do
-      usernames = @comments.map { |c| c["username"] }
-      expect(usernames).to match_array(["foo", "bar"])
+    context "requested without client_key" do
+      before do
+        get "/comments", {location: '/path1'}, { "HTTP_ACCEPT" => "application/json" }
+      end
+
+      it "returns http status 404" do
+        expect(response.status).to eq 404
+      end
     end
+
   end
 end
