6bc8fbc6337c9a76aec0844fe4cceef04d755c63
akahigeg <akahigeg@gmail.com>
2015-03-01 15:15:16 +0900

    add fields and validations to Comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,5 +4,11 @@ class Comment
   field :location, type: String
   field :username, type: String
   field :content, type: String
+  field :mail, type: String
+  field :link, type: String
+  field :remote_ip, type: String
   field :accepted, type: Boolean, default: true
+
+  validates :username, presence: true
+  validates :content, presence: true
 end
--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -10,7 +10,7 @@ describe "Comments API" do
       Comment.create(location: '/path2', username: 'someone', content: 'comment for another path', accepted: true)
     end
 
-    context "requested with valid client_key" do
+    context "when requested with valid client_key" do
       before do
         get "/comments", {location: '/path1', client_key: Setting::client_key}, { "HTTP_ACCEPT" => "application/json" }
         @comments = JSON.parse(response.body)
@@ -30,7 +30,7 @@ describe "Comments API" do
       end
     end
 
-    context "requested comments for another path" do
+    context "when requested comments for another path" do
       before do
         get "/comments", {location: '/path2', client_key: Setting::client_key}, { "HTTP_ACCEPT" => "application/json" }
         @comments = JSON.parse(response.body)
@@ -50,7 +50,7 @@ describe "Comments API" do
       end
     end
 
-    context "requested invalid client_key" do
+    context "when requested invalid client_key" do
       before do
         get "/comments", {location: '/path1', client_key: "invalidkey"}, { "HTTP_ACCEPT" => "application/json" }
       end
@@ -60,7 +60,7 @@ describe "Comments API" do
       end
     end
 
-    context "requested without client_key" do
+    context "when requested without client_key" do
       before do
         get "/comments", {location: '/path1'}, { "HTTP_ACCEPT" => "application/json" }
       end
@@ -70,5 +70,139 @@ describe "Comments API" do
       end
     end
 
+  end
+
+  describe "POST /comments" do
+    before do
+      Comment.delete_all
+      Comment.create(location: '/path1', username: 'bar', content: 'existed comment', accepted: true)
+
+      @comment_params = {
+        location: '/path1',
+        username: 'foo',
+        content: 'oh',
+        mail: 'foo@example.com',
+        link: 'www.example.com'
+      }
+    end
+
+    context "when Setting::default_accepted is true" do
+      before do
+        post "/comments", {comment: @comment_params, client_key: Setting::client_key}, { "HTTP_ACCEPT" => "application/json" }
+        @result = JSON.parse(response.body)
+      end
+
+      it "returns http status 200" do
+        expect(response.status).to eq 200
+      end
+
+      it "created new comment" do
+        expect(Comment.count).to eq 2
+      end
+
+      it "returns username of new comment" do
+        expect(@result["username"]).to eq "foo"
+      end
+
+      it "returns content of new comment" do
+        expect(@result["content"]).to eq "oh"
+      end
+    end
+
+    context "when Setting::default_accepted is false" do
+      before do
+        Setting.first.update_attributes!(default_accepted: false)
+        post "/comments", {comment: @comment_params, client_key: Setting::client_key}, { "HTTP_ACCEPT" => "application/json" }
+        @result = JSON.parse(response.body)
+      end
+
+      after do
+        Setting.first.update_attributes!(default_accepted: true)
+      end
+
+      it "returns http status 200" do
+        expect(response.status).to eq 200
+      end
+
+      it "created new comment" do
+        expect(Comment.count).to eq 2
+      end
+
+      it "returns 'admin' for username of new comment" do
+        expect(@result["username"]).to eq "admin"
+      end
+
+      it "returns waiting message for content of new comment" do
+        expect(@result["content"]).to eq "This comment is waiting to be accepted."
+      end
+    end
+
+    context "when username is blank" do
+      before do
+        @comment_params[:username] = ''
+        post "/comments", {comment: @comment_params, client_key: Setting::client_key}, { "HTTP_ACCEPT" => "application/json" }
+        @result = JSON.parse(response.body)
+      end
+
+      it "returns http status 200" do
+        expect(response.status).to eq 200
+      end
+
+      it "not created new comment" do
+        expect(Comment.count).to eq 1
+      end
+
+      it "returns 'Error' for username of new comment" do
+        expect(@result["username"]).to eq "Error"
+      end
+
+      it "returns error message for content of new comment" do
+        expect(@result["content"]).to eq ["Username can't be blank"]
+      end
+    end
+
+    context "when content is blank" do
+      before do
+        @comment_params[:content] = ''
+        post "/comments", {comment: @comment_params, client_key: Setting::client_key}, { "HTTP_ACCEPT" => "application/json" }
+        @result = JSON.parse(response.body)
+      end
+
+      it "returns http status 200" do
+        expect(response.status).to eq 200
+      end
+
+      it "not created new comment" do
+        expect(Comment.count).to eq 1
+      end
+
+      it "returns 'Error' for username of new comment" do
+        expect(@result["username"]).to eq "Error"
+      end
+
+      it "returns error message for content of new comment" do
+        expect(@result["content"]).to eq ["Content can't be blank"]
+      end
+    end
+
+    context "when requested invalid client_key" do
+      before do
+        post "/comments", {comment: @comment_params, client_key: 'invalidkey'}, { "HTTP_ACCEPT" => "application/json" }
+      end
+
+      it "returns http status 404" do
+        expect(response.status).to eq 404
+      end
+    end
+
+    context "when requested without client_key" do
+      before do
+        post "/comments", {comment: @comment_params}, { "HTTP_ACCEPT" => "application/json" }
+      end
+
+      it "returns http status 404" do
+        expect(response.status).to eq 404
+      end
+    end
   end
 end
